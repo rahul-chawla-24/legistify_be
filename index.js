0c8929f42259b3801790b1e66361677a7922e7a8
Rahul Chawla <63209653+realgcoding@users.noreply.github.com>
2023-07-28 17:10:21 +0530

    🐛fixes

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+const clone = require("clone");
+const data = require("./db.json");
 
+const isProductionEnv = process.env.NODE_ENV === "production";
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(`${__dirname}/tmp`, "db.json"));
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : "db.json", {
+  _isFake: isProductionEnv,
+});
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
 server.use(middlewares);
-server.use(router);
 
-const PORT = 8000;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
+server.use((req, res, next) => {
+  if (req.path !== "/") router.db.setState(clone(data));
+  next();
 });
+
+server.use(router);
+server.listen(process.env.PORT || 8000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
